a1e84c6c9b792d8116b2623e7ae3e28bcf997bd1
amjad-hossain-dsi <amjadict07>
2015-01-21 11:13:17 +0600

    design phase

---
+++ src/com/dsoft/entity/StudentTuitionFee.java
@@ -0,0 +1,15 @@
+package com.dsoft.entity;
+
+/**
+ * Created by amjad on 21/1/15.
+ */
+
+
+public class StudentTuitionFee {
+
+    private Long id;
+
+    private Student student;
+
+    private TuitionFee tuitionFee;
+}
