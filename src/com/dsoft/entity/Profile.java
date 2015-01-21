71255cdc8a157af34b2a539487b79bfdd7d7721d
amjad-hossain-dsi <amjadhossain.dsi>
2015-01-21 08:42:54 +0600

    saving student profile, and get user list

--- src/com/dsoft/entity/Profile.java
+++ src/com/dsoft/entity/Profile.java
@@ -4,4 +4,14 @@ package com.dsoft.entity;
  * Created by amjad on 20/1/15.
  */
 public class Profile {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
